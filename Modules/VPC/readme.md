7f71f062bc023b6bd155784bf77dd235ab3c234e
Chad Bates <cbates255@gmail.com>
2022-10-04 16:00:26 -0400

    coded

---
+++ Modules/VPC/readme.md
@@ -0,0 +1,1 @@
+This module creates a VPC.
--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
