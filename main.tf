07620f71313178e48899032100dfff24727289d6
Chad Bates <cbates255@gmail.com>
2022-10-05 12:16:05 -0400

    project

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
+module "VPC" {
+  source = "./Modules/VPC"
+  vpc_cidr = "10.0.0.0/16"
+  pubsubCIDRblocks = ["10.0.1.0/24", "10.0.2.0/24"]
+  privsubCIDRblocks = ["10.0.3.0/24", "10.0.4.0/24"]
+  azs = ["us-east-1a", "us-east-1b"]
 }
 
-module "VPC" {
-  source = "./Modules/VPC"  
-}
