368ccc38b7855e70c3e35268d7a30afb9ef7c5dc
Chad Bates <cbates255@gmail.com>
2022-10-07 11:10:43 -0400

    bugs

--- Modules/Load Balancer/main.tf
+++ Modules/Load Balancer/main.tf
@@ -46,5 +46,6 @@ resource "aws_security_group" "albsecurity" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -70,6 +70,7 @@ resource "aws_security_group" "databasesecgroup" {
     to_port          = 3306
     protocol         = "tcp"
     security_groups  = [aws_security_group.projectVPCsg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
